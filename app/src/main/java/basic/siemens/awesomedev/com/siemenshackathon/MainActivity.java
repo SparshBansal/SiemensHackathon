19de47af382b2feffa8aedc11f906ea3d90c7e5a
Sparsh Bansal <sparsh.bansal17895@gmail.com>
2017-03-17 16:21:59 +0530

    Initial Commit

---
+++ app/src/main/java/basic/siemens/awesomedev/com/siemenshackathon/MainActivity.java
@@ -0,0 +1,13 @@
+package basic.siemens.awesomedev.com.siemenshackathon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
